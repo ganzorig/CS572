663b109efc5b936a878f6407eca5c1c3991f950c
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2021-07-23 20:22:38 -0500

    Add assignment 2 of day 5

---
+++ assignment5_2/api/routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const controllerGames = require('../controllers/games.controller');
+const controllerPublisher = require('../controllers/publisher.controller');
+
+const router = express.Router();
+
+router
+  .route('/games')
+  .get(controllerGames.gamesGetAll)
+  .post(controllerGames.gamesAddOne);
+
+router
+  .route('/games/:gameId')
+  .get(controllerGames.gamesGetOne)
+  .put(controllerGames.gameFullUpdateOne)
+  .patch(controllerGames.gamePartialUpdateOne)
+  .delete(controllerGames.gameDeleteOne);
+
+router
+  .route('/games/:gameId/publisher')
+  .get(controllerPublisher.publisherGetOne)
+  .post(controllerPublisher.publisherAdd);
+
+module.exports = router;
